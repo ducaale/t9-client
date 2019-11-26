973f16fd03dfa5e89ceaa5691774ad492defe311
ducaale <sharaf.13@hotmail.com>
2019-11-26 11:19:35 +0000

    refactor t9 code and space support

--- src/App.js
+++ src/App.js
@@ -1,56 +1,19 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import Keyboard from './Keyboard'
 import Screen from './Screen'
-import { getPrediction } from './t9Service'
+import useT9 from './useT9'
 import './App.css'
 
 function App() {
-  const [input, setInput] = useState('')
-  const [text, setText] = useState('')
-  const [lastPredictions, setLastPredictions] = useState([])
-  const predictionIndex = useRef(0)
-
-  useEffect(() => {
-    const words = input.split(' ')
-    const lastWord = words[words.length-1]
-    getPrediction(lastWord)
-      .then(predictions => {
-        setLastPredictions(predictions)
-      })
-  }, [input])
-
-  useEffect(() => {
-    predictionIndex.current = 0
-    setText(prev => {
-      const words = prev.split(' ')
-      words[words.length-1] = lastPredictions[0]
-      return words.join('')
-    })
-  }, [lastPredictions])
-
-  const handleInput = char => {
-    setInput(prev => prev + char)
-  }
-  const handleBackSpace = () => {
-    setInput(prev => prev.slice(0,-1))
-  }
-  const handleCycle = () => {
-    if (!lastPredictions.length) { return }
-    setText(prev => {
-      const words = prev.split(' ')
-      predictionIndex.current += 1
-      words[words.length-1] = lastPredictions[predictionIndex.current]
-      return words.join('')
-    })
-  }
+  const { output, addChar, deleteChar, cyclePrediction } = useT9()
 
   return (
     <div className="Body">
-      <Screen text={text} />
+      <Screen text={output} />
       <Keyboard
-        onInput={handleInput}
-        onCycle={handleCycle}
-        onBackspace={handleBackSpace}
+        onInput={addChar}
+        onCycle={cyclePrediction}
+        onBackspace={deleteChar}
       />
     </div>
   );
---
+++ src/useT9.js
@@ -0,0 +1,86 @@
+import { useState, useReducer, useEffect } from 'react';
+import { getPrediction } from './t9Service'
+
+const initialState = { input: [''], output: [''] }
+const IOReducer = (state, action) => {
+  const {input, output } = state
+  switch(action.type) {
+    case 'add_char':
+      if (action.char === ' ') {
+        return {
+          ...state,
+          input: [...input, ''],
+          output: [...output, '']
+        }
+      }
+
+      return {
+        ...state,
+        input: [...input.slice(0, -1), input[input.length-1] + action.char],
+        output: [...output.slice(0, -1), output[output.length-1] + '_']
+      }
+    
+    case 'delete_char':
+      if (input.length === 1 && input[0] === '') {
+        return state
+      }
+
+      if (!input[input.length-1].length) {
+        return {
+          ...state,
+          input: [...input.slice(0, -1)],
+          output: [...output.slice(0, -1)]
+        }
+      }
+
+      return {
+        ...state,
+        input: [...input.slice(0, -1), input[input.length-1].slice(0, -1)],
+        output: [...output.slice(0, -1), output[output.length-1].slice(0, -1)]
+      }
+    
+    case 'update_last_output':
+      if (!input[input.length-1].length) {
+        console.log('should not update an empty word')
+        return state
+      }
+
+      return {
+        ...state,
+        output: [...output.slice(0, -1), action.updatedWord]
+      }
+    default:
+      throw Error(`unknown action type ${action.type}`)
+  }
+}
+
+const useT9 = () => {
+  const [{ input, output }, dispatch] = useReducer(IOReducer, initialState)
+  const [predictions, setPredictions] = useState([])
+  const [predictionIndex, setPredictionIndex] = useState(0)
+
+  useEffect(() => {
+    getPrediction(input[input.length-1]).then(prediction => {
+      setPredictionIndex(0)
+      setPredictions(prediction)
+    })
+  }, [input])
+
+  useEffect(() => {
+    if (predictions[predictionIndex]) {
+      dispatch({
+        type: 'update_last_output',
+        updatedWord: predictions[predictionIndex]
+      })
+    }
+  }, [predictions, predictionIndex])
+
+  return {
+    output: output.join(' '),
+    addChar: char => dispatch({ type: 'add_char', char }),
+    deleteChar: () => dispatch({ type: 'delete_char' }),
+    cyclePrediction: () => setPredictionIndex(prev => prev + 1)
+  }
+}
+
+export default useT9
