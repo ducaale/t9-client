fa60bb542185ef7a671dad5c96eff3788047ef0b
ducaale <sharaf.13@hotmail.com>
2019-12-18 23:45:07 +0000

    make cursor-container same width as text-container

--- src/Screen.js
+++ src/Screen.js
@@ -8,6 +8,9 @@ const Screen = ({ text, cursorPos }) => {
       <span className="Screen-cursor-container">
         {range(text.length + 1).map(i =>
           i === cursorPos ? (
+            // TODO: make this breakable space when text[i] is.
+            // to reproduce the bug type: 'ahmed is going to the staduim'
+            // and move the cursor to pos 21
             <span key={i} className="Screen-cursor">|</span>
           ) : (
             <span key={i} className="Screen-hidden-char">{text[i]}</span>
