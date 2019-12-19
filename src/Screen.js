d638f84e4e6404e2a4c63397bb1ea800ea11c30b
ducaale <sharaf.13@hotmail.com>
2019-12-19 14:13:37 +0000

    implement cursor using border

--- src/Screen.js
+++ src/Screen.js
@@ -4,17 +4,11 @@ import './App.css'
 const Screen = ({ text, cursorPos }) => {
   return (
     <div className="Screen">
-      <span className="Screen-text-container">{text}</span>
-      <span className="Screen-cursor-container">
+      <span className="Screen-text-container">
         {range(text.length + 1).map(i =>
-          i === cursorPos ? (
-            // TODO: make this breakable space when text[i] is.
-            // to reproduce the bug type: 'ahmed is going to the staduim'
-            // and move the cursor to pos 21
-            <span key={i} className="Screen-cursor">|</span>
-          ) : (
-            <span key={i} className="Screen-hidden-char">{text[i]}</span>
-          )
+          <span key={i} className={i === cursorPos ? "Screen-cursor" : ""}>
+            {i < text.length ? text[i] : '\u00A0' }
+          </span>
         )}
       </span>
     </div>
